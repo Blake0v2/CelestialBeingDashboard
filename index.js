a3ef51f320d89fe83cd2939bfed06f6375c07ff7
Blake0v2 <dumboyonpc@outlook.com>
2025-08-12 17:15:09 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,12 +6,3 @@ function showPage(pageId) {
     // Show the selected page
     const selectedPage = document.getElementById(pageId);
     selectedPage.style.display = 'block';
-
-    // Update Dashboard username and avatar dynamically (example)
-    if (pageId === 'dashboard') {
-        const username = "Blake0_o"; // Replace with actual username from Discord OAuth
-        const avatarURL = "https://i.postimg.cc/GhC7KjR7/Hunter-XHunter-GIF-ezgif-com-effects.gif"; // Replace with the actual avatar URL from Discord OAuth
-        document.getElementById('username').textContent = username;
-        document.getElementById('user-avatar').innerHTML = `<img src="${avatarURL}" alt="User Avatar">`;
-    }
-}
